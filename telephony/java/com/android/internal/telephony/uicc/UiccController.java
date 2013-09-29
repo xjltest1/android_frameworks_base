94c629b8e33e2f10f20e7e98ecd11dc9f6225080
xjljian <xjlxxx3@gmail.com>
2013-09-29 13:58:45 +0800

    65

--- telephony/java/com/android/internal/telephony/uicc/UiccController.java
+++ telephony/java/com/android/internal/telephony/uicc/UiccController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (C) 2011-2012 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,75 +16,252 @@
 
 package com.android.internal.telephony.uicc;
 
-import com.android.internal.telephony.IccCard;
-import com.android.internal.telephony.PhoneBase;
-import com.android.internal.telephony.cdma.CDMALTEPhone;
-import com.android.internal.telephony.cdma.CDMAPhone;
-import com.android.internal.telephony.gsm.GSMPhone;
-
+import android.content.Context;
+import android.os.AsyncResult;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Registrant;
+import android.os.RegistrantList;
 import android.util.Log;
+
+import com.android.internal.telephony.CommandsInterface;
+import com.android.internal.telephony.MSimConstants;
+import com.android.internal.telephony.cat.CatService;
 
 /* This class is responsible for keeping all knowledge about
  * ICCs in the system. It is also used as API to get appropriate
  * applications to pass them to phone and service trackers.
  */
-public class UiccController {
-    private static final boolean DBG = true;
+public class UiccController extends Handler {
+    private static final boolean DBG = false;
     private static final String LOG_TAG = "RIL_UiccController";
 
+    public static final int APP_FAM_3GPP =  1;
+    public static final int APP_FAM_3GPP2 = 2;
+    public static final int APP_FAM_IMS   = 3;
+
+    private static final int EVENT_ICC_STATUS_CHANGED = 1;
+    private static final int EVENT_GET_ICC_STATUS_DONE = 2;
+    private static final int EVENT_RADIO_UNAVAILABLE = 3;
+
+    private static final int EVENT_UICC_MGR_INIT_COMPLETE = 4;
+    
     private static UiccController mInstance;
+    private CatService[] mCatService;
+    
+    private Context mContext;
+    private CommandsInterface[] mCi;
+    private UiccCard[] mUiccCards = new UiccCard[MSimConstants.RIL_MAX_CARDS];
 
-    private PhoneBase mCurrentPhone;
-    private boolean mIsCurrentCard3gpp;
-    private IccCard mIccCard;
+    private RegistrantList mIccChangedRegistrants = new RegistrantList();
 
-    public static synchronized UiccController getInstance(PhoneBase phone) {
+    public static synchronized UiccController make(Context c, CommandsInterface[] ci) {
+        if (mInstance != null) {
+            throw new RuntimeException("UiccController.make() should only be called once");
+        }
+        //Log.e(LOG_TAG, " ------------------- > UiccController.make() .... ");
+        mInstance = new UiccController(c, ci);
+        return mInstance;
+    }
+
+    public static synchronized UiccController make(Context c, CommandsInterface ci) {
+        if (mInstance != null) {
+            throw new RuntimeException("UiccController.make() should only be called once");
+        }
+        CommandsInterface[] arrayCi = new CommandsInterface[1];
+        arrayCi[0] = ci;
+        mInstance = new UiccController(c, arrayCi);
+        return mInstance;
+    }
+
+    public static synchronized UiccController getInstance() {
         if (mInstance == null) {
-            mInstance = new UiccController(phone);
-        } else {
-            mInstance.setNewPhone(phone);
+            throw new RuntimeException("UiccController.getInstance can't be called before make()");
         }
         return mInstance;
     }
 
-    public IccCard getIccCard() {
-        return mIccCard;
+    public synchronized UiccCard getUiccCard() {
+        return getUiccCard(MSimConstants.DEFAULT_CARD_INDEX);
     }
 
-    private UiccController(PhoneBase phone) {
-        if (DBG) log("Creating UiccController");
-        setNewPhone(phone);
-    }
-
-    private void setNewPhone(PhoneBase phone) {
-        mCurrentPhone = phone;
-        if (phone instanceof GSMPhone) {
-            if (DBG) log("New phone is GSMPhone");
-            updateCurrentCard(IccCard.CARD_IS_3GPP);
-        } else if (phone instanceof CDMALTEPhone){
-            if (DBG) log("New phone type is CDMALTEPhone");
-            updateCurrentCard(IccCard.CARD_IS_3GPP);
-        } else if (phone instanceof CDMAPhone){
-            if (DBG) log("New phone type is CDMAPhone");
-            updateCurrentCard(IccCard.CARD_IS_NOT_3GPP);
-        } else {
-            Log.e(LOG_TAG, "Unhandled phone type. Critical error!");
+    public synchronized UiccCard getUiccCard(int slotId) {
+        if (slotId >= 0 && slotId < mUiccCards.length) {
+            return mUiccCards[slotId];
         }
+        return null;
     }
 
-    private void updateCurrentCard(boolean isNewCard3gpp) {
-        if (mIsCurrentCard3gpp == isNewCard3gpp && mIccCard != null) {
+    public synchronized UiccCard[] getUiccCards() {
+        // Return cloned array since we don't want to give out reference
+        // to internal data structure.
+        return mUiccCards.clone();
+    }
+
+    // Easy to use API
+    public UiccCardApplication getUiccCardApplication(int family) {
+        return getUiccCardApplication(MSimConstants.DEFAULT_CARD_INDEX, family);
+    }
+
+    public UiccCardApplication getUiccCardApplication(int slotId, int family) {
+        if (slotId >= 0 && slotId < mUiccCards.length) {
+            UiccCard c = mUiccCards[slotId];
+            if (c != null) {
+                return mUiccCards[slotId].getApplication(family);
+            }
+        }
+        return null;
+    }
+
+    // Easy to use API
+    public IccRecords getIccRecords(int family) {
+        return getIccRecords(MSimConstants.DEFAULT_CARD_INDEX, family);
+    }
+
+    public IccRecords getIccRecords(int slotId, int family) {
+        UiccCardApplication app = getUiccCardApplication(slotId, family);
+        if (app != null) {
+            return app.getIccRecords();
+        }
+        return null;
+    }
+
+    // Easy to use API
+    public IccFileHandler getIccFileHandler(int family) {
+        return getIccFileHandler(MSimConstants.DEFAULT_CARD_INDEX, family);
+    }
+
+    public IccFileHandler getIccFileHandler(int slotId, int family) {
+        UiccCardApplication app = getUiccCardApplication(slotId, family);
+        if (app != null) {
+            return app.getIccFileHandler();
+        }
+        return null;
+    }
+
+    //Notifies when card status changes
+    public void registerForIccChanged(Handler h, int what, Object obj) {
+        Registrant r = new Registrant (h, what, obj);
+        mIccChangedRegistrants.add(r);
+        //Notify registrant right after registering, so that it will get the latest ICC status,
+        //otherwise which may not happen until there is an actual change in ICC status.
+        r.notifyRegistrant();
+    }
+    public void unregisterForIccChanged(Handler h) {
+        mIccChangedRegistrants.remove(h);
+    }
+
+    @Override
+    public void handleMessage (Message msg) {
+        Integer index = getCiIndex(msg);
+
+        if (index < 0 || index >= mCi.length) {
+            Log.e(LOG_TAG, "Invalid index : " + index + " received with event " + msg.what);
             return;
         }
 
-        if (mIccCard != null) {
-            mIccCard.dispose();
-            mIccCard = null;
+        switch (msg.what) {
+            case EVENT_ICC_STATUS_CHANGED:
+                Log.e("RIL_UiccController", "Received EVENT_ICC_STATUS_CHANGED, calling getIccCardStatus" + " on index " + index);
+                mCi[index].getIccCardStatus(obtainMessage(EVENT_GET_ICC_STATUS_DONE, index));
+                break;
+            case EVENT_GET_ICC_STATUS_DONE:
+                if (DBG) log("Received EVENT_GET_ICC_STATUS_DONE on index " + index);
+                Log.e("RIL_UiccController", "Received EVENT_GET_ICC_STATUS_DONE on index " + index);
+                AsyncResult ar = (AsyncResult)msg.obj;
+                onGetIccCardStatusDone(ar, index);
+                break;
+            case EVENT_RADIO_UNAVAILABLE:
+                if (DBG) log("EVENT_RADIO_UNAVAILABLE ");
+                Log.e("RIL_UiccController", "EVENT_RADIO_UNAVAILABLE ");
+                disposeCard(index);
+                break;
+            case EVENT_UICC_MGR_INIT_COMPLETE:
+            	 if (DBG) log("EVENT_UICC_MGR_INIT_COMPLETE ");
+            	 Log.e("RIL_UiccController", "EVENT_UICC_MGR_INIT_COMPLETE ");
+            	for (int i = 0; i < this.mCi.length; i++){
+                   Log.d("RIL_UiccManager", "Creating CatService on " + i);
+                   this.mCatService[i] = new CatService(this.mCi[i], this.mContext, i);
+                   if (this.mCatService[i] == null){
+                	   Log.e("RIL_UiccManager", "Couldn't create CatService on " + i);
+                   }
+                 }
+            	break;
+            default:
+            	 Log.e("RIL_UiccController", "Unknown Event " + msg.what);
+                Log.e(LOG_TAG, " Unknown Event " + msg.what);
+        }
+    }
+
+    // Destroys the card object
+    private synchronized void disposeCard(int index) {
+        if (DBG) log("Disposing card");
+        if (mUiccCards[index] != null) {
+            mUiccCards[index].dispose();
+            mUiccCards[index] = null;
+            mIccChangedRegistrants.notifyRegistrants(new AsyncResult(null, index, null));
+        }
+    }
+
+    private UiccController(Context c, CommandsInterface[] ci) {
+        if (DBG) log("Creating UiccController");
+        mContext = c;
+        mCi = ci;
+        mCatService = new CatService[this.mCi.length];
+        for (int i = 0; i < mCi.length; i++) {
+        	Log.e(LOG_TAG, "new UiccController() ....registerForIccStatusChanged()....mCi[i] = " + mCi[i]);
+            Integer index = new Integer(i);
+            mCi[i].registerForIccStatusChanged(this, EVENT_ICC_STATUS_CHANGED, index);
+            mCi[i].registerForNotAvailable(this, EVENT_RADIO_UNAVAILABLE, index);
+            // TODO remove this once modem correctly notifies the unsols
+            mCi[i].registerForOn(this, EVENT_ICC_STATUS_CHANGED, index);
+        }
+        
+       // obtainMessage(EVENT_UICC_MGR_INIT_COMPLETE).sendToTarget();
+    }
+
+    private Integer getCiIndex(Message msg) {
+        AsyncResult ar;
+        Integer index = new Integer(MSimConstants.DEFAULT_CARD_INDEX);
+
+        /*
+         * The events can be come in two ways. By explicitly sending it using
+         * sendMessage, in this case the user object passed is msg.obj and from
+         * the CommandsInterface, in this case the user object is msg.obj.userObj
+         */
+        if (msg != null) {
+            if (msg.obj != null && msg.obj instanceof Integer) {
+                index = (Integer)msg.obj;
+            } else if(msg.obj != null && msg.obj instanceof AsyncResult) {
+                ar = (AsyncResult)msg.obj;
+                if (ar.userObj != null && ar.userObj instanceof Integer) {
+                    index = (Integer)ar.userObj;
+                }
+            }
+        }
+        return index;
+    }
+
+    private synchronized void onGetIccCardStatusDone(AsyncResult ar, Integer index) {
+        if (ar.exception != null) {
+            Log.e(LOG_TAG,"Error getting ICC status. "
+                    + "RIL_REQUEST_GET_ICC_STATUS should "
+                    + "never return an error", ar.exception);
+            return;
+        }
+        
+        IccCardStatus status = (IccCardStatus)ar.result;
+
+        if (mUiccCards[index] == null) {
+            if (DBG) log("Creating a new card");
+            mUiccCards[index] = new UiccCard(mContext, mCi[index], status);
+        } else {
+            if (DBG) log("Update already existing card");
+            mUiccCards[index].update(mContext, mCi[index] , status);
         }
 
-        mIsCurrentCard3gpp = isNewCard3gpp;
-        mIccCard = new IccCard(mCurrentPhone, mCurrentPhone.getPhoneName(),
-                isNewCard3gpp, DBG);
+        if (DBG) log("Notifying IccChangedRegistrants");
+        mIccChangedRegistrants.notifyRegistrants(new AsyncResult(null, index, null));
     }
 
     private void log(String string) {
